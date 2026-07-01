2024d0878c37efd0abb9c56b003204967f3c8db8
Sasadism <104597434+Sasadism@users.noreply.github.com>
2026-07-02 03:05:24 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,1 +1,23 @@
+#!/bin/sh
 
+set -e
+
+mkdir -p /app
+
+cd /app
+
+echo "Downloading latest 3X-UI..."
+
+ARCH="linux-amd64"
+
+URL="https://github.com/MHSanaei/3x-ui/releases/latest/download/x-ui-${ARCH}.tar.gz"
+
+curl -L "$URL" -o x-ui.tar.gz
+
+tar -xzf x-ui.tar.gz
+
+chmod +x x-ui
+
+echo "Starting 3X-UI..."
+
+./x-ui
